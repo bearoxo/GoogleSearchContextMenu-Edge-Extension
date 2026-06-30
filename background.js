a2d589787a1c327f85b536569ec165d1ba876649
bearoxo <a.y.banzai@gmail.com>
2026-07-01 06:41:33 +0800

    added localization params

--- background.js
+++ background.js
@@ -3,13 +3,13 @@ const CONTEXT_MENU_ID = 'contextMenu_SearchWithGoogle';
 
 const locale = (navigator.language || "en-US").replace('_', '-'); 
 const parts = locale.split('-');
-const hl = parts[0] ? parts[0].toLowerCase() : 'en'; ; // e.g., "en"
+const hl = parts[0] ? parts[0] : 'en'; ; // e.g., "en"
 const gl = parts[1] ? parts[1].toLowerCase() : 'us'; // e.g., "us"
 
 function buildSearchUrl(text) {
 	let url = `https://www.google.com/search?q=${encodeURIComponent(text)}`;
 	url += `&hl=${hl}`;
-  url += `&gl=${gl}`;
+	url += `&gl=${gl}`;
 	url += `&sourceid=chrome`;
 	url += `&ie=UTF-8`;
 
@@ -39,9 +39,9 @@ function setupContextMenu() {
 browser.runtime?.onInstalled?.addListener(setupContextMenu);
 browser.runtime?.onStartup?.addListener(setupContextMenu);
 
-browser.contextMenus.onClicked.addListener((info, tab) => {
+browser.contextMenus.onClicked.addListener((info) => {
 	if (info.menuItemId === CONTEXT_MENU_ID) {
-		handleSearchSelection(info, tab);
+		handleSearchSelection(info);
 	}
 });
 
